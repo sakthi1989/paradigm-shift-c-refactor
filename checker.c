3247f39954c2d83c796990673774bfa3b07f0a60
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-08-11 20:52:54 +0530

    checker.c

--- checker.c
+++ checker.c
@@ -1,18 +1,53 @@
 #include <stdio.h>
 #include <assert.h>
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
-  }
-  return 1;
+typedef enum {
+    ENGLISH,
+    GERMAN
+} Language;
+
+Language language = ENGLISH;
+
+void PrintMessage(const char* msg_en, const char* msg_de)
+{
+    switch (language) 
+    {
+        case 0:
+            printf("EN: %s\n", msg_en);
+            break;
+        case 1:
+            printf("DE: %s\n", msg_de);
+            break;
+        default:
+            printf("Language not supported\n");
+            break;
+    }
+}
+
+int OutOfRange(float val, float min, float max, const char* msg_en, const char* msg_de) 
+{
+    if (val < min || val > max)
+    {
+        PrintMessage(msg_en, msg_de);
+        return 0;
+    }
+    return 1;
+}
+
+int batteryIsOk(float temperature, float soc, float chargeRate)
+{
+  // Messages in English and German
+    const char* msg_temp_en = "Temperature out of range!";
+    const char* msg_soc_en = "SOC out of range!";
+    const char* msg_charge_en = "Charge Rate out of range!";
+    
+    const char* msg_temp_de = "Temperatur außerhalb des zulässigen Bereichs!";
+    const char* msg_soc_de = "SOC außerhalb des zulässigen Bereichs!";
+    const char* msg_charge_de = "Ladestrom außerhalb des zulässigen Bereichs!";
+    
+    return OutOfRange(temperature, 0, 45, msg_temp_en, msg_temp_de)
+        && OutOfRange(soc, 20, 80, msg_soc_en, msg_soc_de)
+        && OutOfRange(chargeRate, 0, 0.8, msg_charge_en, msg_charge_de);
 }
 
 int main() {
